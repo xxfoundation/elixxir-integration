1f8ce25cfafca2e6bd9b148f68f763c43c46a06e
Rick Carback <rick@privategrity.com>
2018-03-06 22:50:57 +0000

    final touches for initial integration test

--- basice2e/run.sh
+++ basice2e/run.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+rm -fr results || true
+
 SERVERLOGS=results/servers
 CLIENTOUT=results/clients
 
@@ -16,17 +18,23 @@ for SERVERID in $(seq 1 5)
 do
     IDX=$(($SERVERID - 1))
     SERVERCMD="../bin/server -v -i $IDX --config server-$SERVERID.yaml"
-    eval $SERVERCMD > $SERVERLOGS/server-$SERVERID.console 2>&1 &
-    echo "$SERVERCMD -- $!"
+    $SERVERCMD > $SERVERLOGS/server-$SERVERID.console 2>&1 &
+    RETVAL=$!
+    echo "$SERVERCMD -- $RETVAL"
 done
+
+jobs -p > results/serverpids
 
 finish() {
     echo "STOPPING SERVERS..."
-    for job in $(jobs -p)
+    # NOTE: jobs -p doesn't work in a signal handler
+    for job in $(cat results/serverpids)
     do
+        echo "KILLING $job"
         kill $job || true
     done
-    tail results/*
+    tail $SERVERLOGS/*
+    tail $CLIENTOUT/*
 }
 
 trap finish EXIT
@@ -41,25 +49,31 @@ LASTNODE="localhost:50004"
 echo "STARTING CLIENTS..."
 CTR=0
 
-for cid in 1 2 3 4; do
+for cid in $(seq 1 4)
+do
     # TODO: Change the recipients to send multiple messages. We can't
     #       run multiple clients with the same user id so we need
     #       updates to make that work.
     #     for nid in 1 2 3 4; do
-    for nid in 1; do
+    for nid in 1
+    do
         nid=$((($cid % 4) + 1))
-        ../bin/client --numnodes 5 -s $LASTNODE -i $cid -d $nid -m "Hello, $nid" > results/client$cid$nid.out 2>&1 &
+        CLIENTCMD="../bin/client --numnodes 5 -s $LASTNODE -i $cid -d $nid -m \"Hello, $nid\""
+        eval $CLIENTCMD > $CLIENTOUT/client$cid$nid.out 2>&1 &
         RETVAL=$!
-        echo "../bin/client --numnodes 5 -s $LASTNODE -i $cid -d $nid -m \"Hello, $nid\" -- $RETVAL"
         eval CLIENTS${CTR}=$RETVAL
+        echo "$CLIENTCMD -- $RETVAL"
         CTR=$(($CTR + 1))
     done
 done
 
 echo "WAITING FOR $CTR CLIENTS TO EXIT..."
-for i in $(seq 0 $(($CTR - 1))); do
+for i in $(seq 0 $(($CTR - 1)))
+do
     eval echo "Waiting on \${CLIENTS${i}} ..."
     eval wait \${CLIENTS${i}}
 done
 
-diff -ruN 
+diff -ruN clients.goldoutput $CLIENTOUT
+
+echo "SUCCESS!"
