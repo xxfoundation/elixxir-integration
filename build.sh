378696c83190bf11da1d8e838316e5e4a9207e6c
Spencer Brown <spencer@privategrity.com>
2019-02-14 14:57:06 -0800

    Add TLS support and use different user IDs

--- build.sh
+++ build.sh
@@ -6,33 +6,34 @@
 
 mkdir -p bin
 
+build() {
+    #go generate cmd/version.go
+    go build
+}
+
 pushd $GOPATH/src/gitlab.com/elixxir/client
-go generate cmd/version.go
-go build
+build
 popd
 mv $GOPATH/src/gitlab.com/elixxir/client/client bin
 
 pushd $GOPATH/src/gitlab.com/elixxir/server
-go generate cmd/version.go
-go build
+build
 popd
 mv $GOPATH/src/gitlab.com/elixxir/server/server bin
 
 pushd $GOPATH/src/gitlab.com/elixxir/channelbot
-go generate cmd/version.go
-go build
+build
 popd
 mv $GOPATH/src/gitlab.com/elixxir/channelbot/channelbot bin
 
 UDBPATH=gitlab.com/elixxir/user-discovery-bot
 pushd $GOPATH/src/$UDBPATH
-go generate cmd/version.go
+#go generate cmd/version.go
 popd
 go build -o udb $UDBPATH
 mv ./udb bin
 
 pushd $GOPATH/src/gitlab.com/elixxir/gateway
-go generate cmd/version.go
-go build
+build
 popd
 mv $GOPATH/src/gitlab.com/elixxir/gateway/gateway bin
