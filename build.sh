8059dce77cdde27fbbe7d369126b70e6cde90e9b
Spencer Brown <spencer@privategrity.com>
2019-02-18 11:27:20 -0800

    Clean up build script

--- build.sh
+++ build.sh
@@ -5,35 +5,10 @@
 # with Glide.
 
 mkdir -p bin
-
-build() {
-    #go generate cmd/version.go
-    go build
-}
-
-pushd $GOPATH/src/gitlab.com/elixxir/client
-build
+pushd bin
+go build gitlab.com/elixxir/client
+go build gitlab.com/elixxir/channelbot
+go build -o udb gitlab.com/elixxir/user-discovery-bot
+go build gitlab.com/elixxir/gateway
+go build gitlab.com/elixxir/server
 popd
-mv $GOPATH/src/gitlab.com/elixxir/client/client bin
-
-pushd $GOPATH/src/gitlab.com/elixxir/server
-build
-popd
-mv $GOPATH/src/gitlab.com/elixxir/server/server bin
-
-pushd $GOPATH/src/gitlab.com/elixxir/channelbot
-build
-popd
-mv $GOPATH/src/gitlab.com/elixxir/channelbot/channelbot bin
-
-UDBPATH=gitlab.com/elixxir/user-discovery-bot
-pushd $GOPATH/src/$UDBPATH
-#go generate cmd/version.go
-popd
-go build -o udb $UDBPATH
-mv ./udb bin
-
-pushd $GOPATH/src/gitlab.com/elixxir/gateway
-build
-popd
-mv $GOPATH/src/gitlab.com/elixxir/gateway/gateway bin
