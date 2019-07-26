8867a77420e814e85014aa5a94811cf053eba013
Rick Carback <rick@privategrity.com>
2019-07-26 01:03:04 +0000

    Add registration/permissioning server to build script

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ mkdir -p bin
 pushd bin
 go build gitlab.com/elixxir/client
 go build -o udb gitlab.com/elixxir/user-discovery-bot
+go build -o permissioning gitlab.com/elixxir/registration
 go build gitlab.com/elixxir/gateway
 go build gitlab.com/elixxir/server
 popd
