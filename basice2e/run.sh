6e1044450cab2cca3d85b9173b4dfc353241edfc
Jono Wenger <jono@elixxir.io>
2022-03-09 11:09:42 -0800

    Fix openssl failing for invalid addresses due to line-endings in Windows

--- basice2e/run.sh
+++ basice2e/run.sh
@@ -173,7 +173,7 @@ else
 fi
 
 echo "DOWNLOADING TLS Cert..."
-CMD="openssl s_client -showcerts -connect $(cat results/startgwserver.txt)"
+CMD="openssl s_client -showcerts -connect $(tr -d '[:space:]' < results/startgwserver.txt)"
 echo $CMD
 eval $CMD < /dev/null 2>&1 > "results/startgwcert.bin"
 CMD="cat results/startgwcert.bin | openssl x509 -outform PEM"
@@ -182,7 +182,7 @@ eval $CMD > "results/startgwcert.pem"
 head "results/startgwcert.pem"
 
 echo "DOWNLOADING NDF..."
-CLIENTCMD="../bin/client getndf --gwhost $(cat results/startgwserver.txt) --cert results/startgwcert.pem"
+CLIENTCMD="../bin/client getndf --gwhost $(tr -d '[:space:]' < results/startgwserver.txt) --cert results/startgwcert.pem"
 eval $CLIENTCMD >> results/ndf.json 2>&1 &
 PIDVAL=$!
 echo "$CLIENTCMD -- $PIDVAL"
