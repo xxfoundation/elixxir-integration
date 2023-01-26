bd1b8d923c6a25ca1b22fdc5e46799b2243ba910
joshemb <josh@elixxir.io>
2023-01-26 13:32:49 -0800

    Add more ignore messages for HTTP/GRPC error messages

--- basice2e/run.sh
+++ basice2e/run.sh
@@ -1355,7 +1355,10 @@ then
     cat $SERVERLOGS/server-*.log | grep -a "FATAL" | grep -a -v "context" | grep -av "transport is closing" | grep -av "database" >> results/server-errors.txt || true
     diff -aruN results/server-errors.txt noerrors.txt
     IGNOREMSG="GetRoundBufferInfo: Error received: rpc error: code = Unknown desc = round buffer is empty"
-    cat $GATEWAYLOGS/*.log | grep -a "ERROR" | grep -av "context" | grep -av "certificate" | grep -av "Failed to read key" | grep -av "$IGNOREMSG" | grep -av "Failed to serve port" | grep -av "Failed to start HTTPS" > results/gateway-errors.txt || true
+    IGNORESERVEPORT="Failed to serve port"
+    IGNORESERVEHTTP="Failed to serve HTTP"
+    IGNORESERVEGRPC="Failed to serve GRPC"
+    cat $GATEWAYLOGS/*.log | grep -a "ERROR" | grep -av "context" | grep -av "certificate" | grep -av "Failed to read key" | grep -av "$IGNOREMSG" | grep -av IGNORESERVEGRPC | grep -av IGNORESERVEHTTP | grep -av IGNORESERVEPORT | grep -av "Failed to start HTTPS" > results/gateway-errors.txt || true
     cat $GATEWAYLOGS/*.log | grep -a "FATAL" | grep -av "context" | grep -av "transport is closing" >> results/gateway-errors.txt || true
     diff -aruN results/gateway-errors.txt noerrors.txt
     echo "Checking backup files for equality..."
